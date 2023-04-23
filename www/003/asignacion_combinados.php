8371325dddd2bd454f70e0d59d79e6f3177fa31f
rbuj <robert.buj@gmail.com>
2023-04-23 23:02:23 +0200

    Corrige error tipográfico

--- www/003/asignacion_combinados.php
+++ www/003/asignacion_combinados.php
@@ -1,6 +1,6 @@
 <?php
 $a = 23;
-$b = $a++; // $b es igual a 23, porque $a se incremeta después de la asignación
+$b = $a++; // $b es igual a 23, porque $a se incrementa después de la asignación
 
 $a = 23;
 $c = ++$a; // $c es igual a 24, porque $a se incrementa antes de la asignación
